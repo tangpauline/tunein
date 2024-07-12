1c99fb056cde94f668dd89956702983a5b363b33
Pauline Tang <paulinetang1@berkeley.edu>
2024-07-12 13:16:44 -0700

    modal added, alert added

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,38 @@
 import './assets/home.css'
 import {useRouter} from "next/navigation";
 import Footer from './components/footer';
+import Modal from 'react-modal';
+import {useState} from 'react';
 
 export const fetchCache = 'force-no-store';
 
+// modal styling
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#FFD0EC',
+    color: '#474F7A',
+    textAlign: 'center',
+    borderRadius: '20px',
+    boxShadow: "0px 0px 40px #474F7A",
+  },
+  overlay: {
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+  },
+};
+
 export default function Home() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const router = useRouter();
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  }
 
   const routeLogin = () => {
     router.push("/api/login")
@@ -14,13 +41,22 @@ export default function Home() {
 
   return (
     <main>
-      <button className="login-btn" id="b1" onClick={routeLogin}>Log in</button>
       <div className="header">
         <h1>TuneIn</h1>
         <h2>Spotify Music Dashboard</h2>
         <p className="login-desc">Log in to get started</p>
-        <p className="login-desc-p">(**Please note this app is currently in development mode. <br></br>For access, please contact me at tangpauline1@gmail if interested**)</p>
-        <button className="login-btn" id="b2" onClick={routeLogin}>Log in</button>
+        <button className="login-btn" id="b2" onClick={openModal}>Log in</button>
+
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => {setModalIsOpen(false)}}
+          style={customStyles}
+          ariaHideApp={false}
+        >
+          <p>Please note this app is currently in development mode. <br></br>For access, please contact me at <strong>tangpauline1@gmail</strong> if interested. <br></br>Sorry for the inconvenience!</p>
+          <button className="back-btn" onClick={() => {setModalIsOpen(false)}}>Back</button>
+          <button className="proceed-btn" onClick={routeLogin}>Proceed</button>
+        </Modal>
       </div>
       <Footer></Footer>
     </main>
